f45b85bfdb40151e7a3e211178b4f50de7407a5b
riain <ryanftoal@gmail.com>
2024-06-03 00:36:50 +0100

    'No dictionary found' error handling implemented.

--- interface/process.py
+++ interface/process.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QMainWindow, QFileDialog, QMessageBox
 from pathlib import Path
 from utils.handle_file import discern_type_all, discern_type_scan, discern_type_spellcheck
 from utils.error import error_dispatcher
+from utils.system import filetype_in_directory
 from .help import HelpWindow
 
 
@@ -13,6 +14,13 @@ class Processes(QMainWindow):
         self.help = None
 
     def _open_file(self) -> None:
+        if not filetype_in_directory(Path('dictionaries'), '.txt'):
+            error_dispatcher.raise_error(
+                'Dictionary not found',
+                "No dictionaries found.\nUse 'Dictionary' tab to load dictionaries."
+            )
+            return
+
         files, _ = QFileDialog.getOpenFileNames(
             self,
             'Open',
@@ -20,11 +28,11 @@ class Processes(QMainWindow):
             'All files (*.jpg *.png *.txt *.pdf);;Images (*.jpg *.png);;Text (*.txt);;PDF (*.pdf)',
             options=QFileDialog.Options()
         )
+
         if files:
             for file in files:
                 path = Path(file)
-                if not discern_type_all(path):
-                    error_dispatcher.raise_error()  # what's going on with this syntax?
+                discern_type_all(path)
 
         return
 
@@ -36,6 +44,7 @@ class Processes(QMainWindow):
             'All files (*.jpg *.png *.pdf);;Images (*.jpg *.png);;PDF (*.pdf)',
             options=QFileDialog.Options()
         )
+
         if files:
             for file in files:
                 path = Path(file)
@@ -44,6 +53,13 @@ class Processes(QMainWindow):
         return
 
     def _open_spellcheck(self) -> None:
+        if not filetype_in_directory(Path('dictionaries'), '.txt'):
+            error_dispatcher.raise_error(
+                'Dictionary not found',
+                "No dictionaries found.\nUse 'Dictionary' tab to load dictionaries."
+            )
+            return
+
         options = QFileDialog.Options()
         files, _ = QFileDialog.getOpenFileNames(
             self,
@@ -52,6 +68,7 @@ class Processes(QMainWindow):
             'All files (*.txt);;Text files (*.txt)',
             options=options
         )
+
         if files:
             for file in files:
                 path = Path(file)
